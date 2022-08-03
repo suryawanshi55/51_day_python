41fafc2011f9548dc63bd35c7841fcb95b92f091
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-03 14:56:15 +0530

    Add files via upload

---
+++ function_return_several_value.py
@@ -0,0 +1,12 @@
+# def fv (n1,n2):          # 3, 6
+#     a=n1+n2         # 9.0
+#     b=n1-n2         #-3.0
+#     c=n1*n2          #18.0
+#     d=n1/n2         # 0.5
+#     return a,b,c,d        #  9.0 -3.0 18.0 0.5
+# x=float(input("enter the value of x:"))       #3
+# y=float(input("enter the value of y:"))         #6
+# Ar=fv(x,y)            #9.0
+# print(Ar)             #-3.0
+# print(*Ar)             #18.0
+# for i in Ar : print(i)           #0.5
